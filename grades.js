73db9e4569dbb2c7a0a6981933dd7707b4be2ad0
Megan B <brown.m.j@outlook.com>
2017-06-07 16:59:34 -0500

    completed instructions for exercise

--- grades.js
+++ grades.js
@@ -1,3 +1,4 @@
+console.log("hello Megan");
 // Requirements
 
 // Loop over an array of student grades (values from 50-100) 
@@ -16,5 +17,48 @@ var scores = [82, 71, 95, 55, 98, 69, 72, 78, 84, 64, 58, 87];
 // Use console.log() to output the following criteria:
 
 // How many of each grade?
+countA = 0;
+countB = 0;
+countC = 0;
+countD = 0;
+countF = 0;
+
+for (i = 0; i < scores.length; i++) {
+	if (scores[i] >= 50 && scores[i] <= 60) {
+		countF++;
+	} else if (scores[i] >= 61 && scores[i] <= 70) {
+		countD++;
+	} else if (scores[i] >= 71 && scores[i] <= 80) {
+		countC++;
+	} else if (scores[i] >= 81 && scores[i] <= 90) {
+		countB++;
+	} else if (scores[i] >= 91 && scores[i] <= 100) {
+		countA++;
+	}
+}
+console.log("Number of F's = ", countF);
+console.log("Number of D's = ", countD);
+console.log("Number of C's = ", countC);
+console.log("Number of B's = ", countB);
+console.log("Number of A's = ", countA);
+
 // What is the lowest grade?
+var scoresOrder = scores.sort(function(first, second) {
+	return first - second;
+});
+
+var lowest = scoresOrder.shift();
+console.log("The lowest grade is " + lowest);
+
 // What is the highest grade?
+var highest = scoresOrder.pop();
+console.log("The highest grade is " + highest);
+
+
+
+
+
+
+
+
+
